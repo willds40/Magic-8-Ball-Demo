aaf6edc920537e8130df5440ae965bfae9384520
wdsand <willds40@gmail.com>
2019-09-12 09:22:45 -0700

    adds extra imports to be used in hte test

--- __tests__/components/App.spec.js
+++ __tests__/components/App.spec.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render } from 'react-native-testing-library';
 import App from '../../App';
+import * as response from '../../responses';
+import { toBeEmpty, toHaveTextContent } from '@testing-library/jest-native';
+expect.extend({ toBeEmpty, toHaveTextContent });
 
 describe('App', () => {
     it('renders the correct title', () => {
