8ef0b869522cb3ae9786a11c7b4d509798591ba0
wdsand <willds40@gmail.com>
2019-12-27 11:47:48 -0500

    update tests

--- App.js
+++ App.js
@@ -4,10 +4,31 @@ import {getResponse} from './responses';
 import style from './styles'
 
 const App = () => {
+    const [text, setState] = useState("");
     return (
-        <View style={style.titleView}>
-            <Text style={style.title}>Maigc 8 Ball</Text>
-        </View>
+        <React.Fragment>
+            <View style={style.titleView}>
+                <Text style={style.title}>Maigc 8 Ball</Text>
+            </View>
+            <ImageBackground
+                style={style.imageBackground}
+                source={require('./assets/triangle-1.png')}
+            >
+                <View style={style.textView}>
+                    <Button
+                        title="Click Me"
+                        color="black"
+                        testID="response-button"
+                        onPress={(() => {
+                            setState(getResponse())
+                        })}
+                    >
+                    </Button>
+                    <Text testID="response-text" style={style.response}>{text}</Text>
+                </View>
+            </ImageBackground>
+        </React.Fragment>
+        
     );
 }
 
--- __tests__/components/App.spec.js
+++ __tests__/components/App.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-native-testing-library';
+import { render, fireEvent} from 'react-native-testing-library';
 import App from '../../App';
 import * as response from '../../responses';
 import { toBeEmpty, toHaveTextContent } from '@testing-library/jest-native';
@@ -10,4 +10,23 @@ describe('App', () => {
         const { queryByText } = render(<App />);
         expect(queryByText('Maigc 8 Ball')).not.toBeNull();
     });
+
+    it('renders the correct button title', () => {
+        const { queryByText } = render(<App />);
+        expect(queryByText('Click Me')).not.toBeNull();
+    });
+
+    it('response text is an empty string on start', () => {
+        const { getByTestId } = render(<App />);
+        expect(getByTestId('response-text')).toHaveTextContent(''); ``
+    });
+
+    it('get response is called once and return never ever', () => {
+        response.getResponse = jest.fn().mockImplementationOnce(() => "Never Ever");
+        const { getByTestId, getByText } = render(<App />);
+        const responseButton = getByTestId('response-button')
+        fireEvent.press(responseButton)
+        expect(response.getResponse).toHaveBeenCalled()
+        expect(getByText("Never Ever")).not.toBeNull()       
+    });    
 });
